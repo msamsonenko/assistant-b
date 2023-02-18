b057ee4fab515f79066bb44bcff5f3538e41c1c2
IrynaSam <52596926+msamsonenko@users.noreply.github.com>
2023-02-18 11:28:43 +0200

    password hash on register added

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,13 +1,15 @@
 const { User } = require("../../models/user");
 const { createError } = require("../../helpers");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res, next) => {
-	const { email } = req.body;
+	const { email, password } = req.body;
 	const user = await User.findOne({ email });
 	if (user) {
 		throw createError(409, "Email is in use");
 	}
-	const result = await User.create(req.body);
+	const hashPassword = await bcrypt.hash(password, 10);
+	const result = await User.create({ ...req.body, password: hashPassword });
 	res.status(201).json({
 		email: result.email,
 		name: result.name,
