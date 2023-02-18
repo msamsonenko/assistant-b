11dad8c39db6d52b26b97d8aee907843575aa120
IrynaSam <52596926+msamsonenko@users.noreply.github.com>
2023-02-18 12:15:48 +0200

    login controller add

---
+++ controllers/auth/login.js
@@ -0,0 +1,24 @@
+const { User } = require("../../models/user");
+const { createError } = require("../../helpers");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = process.env;
+
+const login = async (req, res) => {
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (!user) {
+		throw createError(401, "Email or password is invalid");
+	}
+	if (!bcrypt.compare(password, user.password)) {
+		throw createError(401, "Email or password is invalid");
+	}
+	const payload = {
+		id: user._id,
+	};
+	const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+	res.json({ token });
+};
+
+module.exports = login;
